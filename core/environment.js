53a6755142f4410802ccb69609f2b64c8e822b20
Chris Hale <chris.hale@me.com>
2018-06-29 15:44:47 -0600

    more updates

--- core/environment.js
+++ core/environment.js
@@ -1,1 +1,25 @@
 //var mode   = process.env.NODE_ENV;
+//'a.b.etc'.split('.').reduce(index, obj);
+/**
+ * rightmost object wins
+ * const person1 = { name: 'David Walsh', age: 33 };
+const person2 = { name: 'David Walsh Jr.', role: 'kid' };
+
+const merged = {...person1, ...person2}
+ */
+
+// var glob = require( 'glob' )
+//   , path = require( 'path' );
+//   const ROOT = path.resolve('./')
+// const configDir = path.join(ROOT,'config/')
+// glob.sync( `${configDir}**/*.yml` ).forEach( function( file, index ) {
+//  // require( path.resolve( file ) )
+// /*  global.test = file
+//  log.info(global.test)
+//  log.info(index) */
+//  log.info(file)
+// });
+const config = require("config");
+config.blah = 'cool'
+console.log(config.get('Customer'));
+module.exports = config;
--- core/index.js
+++ core/index.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
-
+const config = require('./environment')
 yaml = require('js-yaml');
 fs   = require('fs');
-log = require('./log');
 // SETUP LOGGING
+log = require('./log');
 
+
+log.info(config.get('blah'))
 // Get document, or throw exception on error
-try {
-  var doc = yaml.safeLoad(fs.readFileSync(__dirname+'/../config/test.yml', 'utf8'));
-  log.info(doc);
-} catch (e) {
-  console.log(e);
-}
-
-
-
-
-
 
 app.get('/', function(req, res){
+
+  /**
+   * class Hero {
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    // Adding a method to the constructor
+    greet() {
+        return `${this.name} says hello.`;
+    }
+}
+   */
    res.send("Hello world!");
 });
 
